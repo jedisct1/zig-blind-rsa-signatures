a2ecfc9520d3d8663940cbfc518ea07acfa7e2f4
Frank Denis <github@pureftpd.org>
2024-05-09 21:54:56 +0200

    Update for Zig 0.13

--- build.zig
+++ build.zig
@@ -9,24 +9,24 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const lib = b.addStaticLibrary(.{
         .name = "rsa-blind-signatures",
-        .root_source_file = .{ .path = "src/main.zig" },
+        .root_source_file = b.path("src/main.zig"),
         .optimize = optimize,
         .target = target,
     });
     b.installArtifact(lib);
 
-    var main_tests = b.addTest(.{ .root_source_file = .{ .path = "src/main.zig" } });
+    var main_tests = b.addTest(.{ .root_source_file = b.path("src/main.zig") });
 
     if (crypto_lib == .openssl) {
-        main_tests.addSystemIncludePath(.{ .path = "/usr/local/opt/openssl/include" });
-        main_tests.addSystemIncludePath(.{ .path = "/usr/local/openssl/include" });
-        main_tests.addSystemIncludePath(.{ .path = "/opt/homebrew/opt/openssl@3/include" });
-        main_tests.addLibraryPath(.{ .path = "/usr/local/opt/openssl/lib" });
-        main_tests.addLibraryPath(.{ .path = "/usr/local/openssl/lib" });
-        main_tests.addLibraryPath(.{ .path = "/opt/homebrew/opt/openssl@3/lib" });
+        main_tests.addSystemIncludePath(.{ .cwd_relative = "/usr/local/opt/openssl/include" });
+        main_tests.addSystemIncludePath(.{ .cwd_relative = "/usr/local/openssl/include" });
+        main_tests.addSystemIncludePath(.{ .cwd_relative = "/opt/homebrew/opt/openssl@3/include" });
+        main_tests.addLibraryPath(.{ .cwd_relative = "/usr/local/opt/openssl/lib" });
+        main_tests.addLibraryPath(.{ .cwd_relative = "/usr/local/openssl/lib" });
+        main_tests.addLibraryPath(.{ .cwd_relative = "/opt/homebrew/opt/openssl@3/lib" });
     } else {
-        main_tests.addSystemIncludePath(.{ .path = "/Users/j/src/boringssl/install/include" });
-        main_tests.addLibraryPath(.{ .path = "/Users/j/src/boringssl/install/lib" });
+        main_tests.addSystemIncludePath(.{ .cwd_relative = "/Users/j/src/boringssl/install/include" });
+        main_tests.addLibraryPath(.{ .cwd_relative = "/Users/j/src/boringssl/install/lib" });
     }
     main_tests.linkSystemLibrary("crypto");
 
