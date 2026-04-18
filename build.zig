36d6a1e53b3d1984ba51d0ec81b73766b9a0d45f
Øystein Olsen <oysols@gmail.com>
2026-04-19 00:02:55 +0200

    Add support for using as a zig package

--- build.zig
+++ build.zig
@@ -1,9 +1,5 @@
 const std = @import("std");
 
-const CryptoLib = enum { openssl, boringssl };
-
-const crypto_lib: CryptoLib = .openssl;
-
 pub fn build(b: *std.Build) void {
     const optimize = b.standardOptimizeOption(.{});
     const target = b.standardTargetOptions(.{});
@@ -13,40 +9,25 @@ pub fn build(b: *std.Build) void {
         .target = target,
         .optimize = optimize,
     }).createModule();
+    ssl.linkSystemLibrary("crypto", .{});
+
+    const root_module = b.addModule("rsa-blind-signatures", .{
+        .root_source_file = b.path("src/main.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    root_module.addImport("ssl", ssl);
 
     const lib = b.addLibrary(.{
         .name = "rsa-blind-signatures",
-        .root_module = b.createModule(.{
-            .root_source_file = b.path("src/main.zig"),
-            .target = target,
-            .optimize = optimize,
-        }),
+        .root_module = root_module,
         .linkage = .static,
     });
-    lib.root_module.addImport("ssl", ssl);
     b.installArtifact(lib);
 
-    var main_tests = b.addTest(.{
-        .root_module = b.createModule(.{
-            .root_source_file = b.path("src/main.zig"),
-            .target = target,
-            .optimize = optimize,
-        }),
+    const main_tests = b.addTest(.{
+        .root_module = root_module,
     });
-    main_tests.root_module.addImport("ssl", ssl);
-
-    if (crypto_lib == .openssl) {
-        main_tests.root_module.addSystemIncludePath(.{ .cwd_relative = "/usr/local/opt/openssl/include" });
-        main_tests.root_module.addSystemIncludePath(.{ .cwd_relative = "/usr/local/openssl/include" });
-        main_tests.root_module.addSystemIncludePath(.{ .cwd_relative = "/opt/homebrew/opt/openssl@3/include" });
-        main_tests.root_module.addLibraryPath(.{ .cwd_relative = "/usr/local/opt/openssl/lib" });
-        main_tests.root_module.addLibraryPath(.{ .cwd_relative = "/usr/local/openssl/lib" });
-        main_tests.root_module.addLibraryPath(.{ .cwd_relative = "/opt/homebrew/opt/openssl@3/lib" });
-    } else {
-        main_tests.root_module.addSystemIncludePath(.{ .cwd_relative = "/Users/j/src/boringssl/install/include" });
-        main_tests.root_module.addLibraryPath(.{ .cwd_relative = "/Users/j/src/boringssl/install/lib" });
-    }
-    main_tests.root_module.linkSystemLibrary("crypto", .{});
 
     const run_main_tests = b.addRunArtifact(main_tests);
 
